795d2987c8b40b6bd02b63a4c9eba72e6b26b2c8
sleaxdyy <84704651+sleaxdyy@users.noreply.github.com>
2023-04-25 17:48:45 +0700

    Update sendTelegram.groovy

--- vars/sendTelegram.groovy
+++ vars/sendTelegram.groovy
@@ -1,5 +1,5 @@
 def call(String config){
 def botUrl = "https://api.telegram.org/bot6105612158:AAES_JhvfeoZOBo3koQxaq9e-ZQphRw_Alc/sendMessage"
-def bot = { text -> sh "curl -s -X POST ${botUrl} -d chat_id=-763851020 -d text='${text}'" }
+def bot = { text -> sh "curl -s -X POST ${botUrl} -d chat_id=-1001902908936 -d text='${text}'" }
   bot("${config}")
 }
